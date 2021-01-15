41a0fc4bb486657295cb60188fe81a3cc53acc63
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2021-01-15 16:52:33 +0100

    Adding first working version
    The program creates a visualisation of Pascal's triangle based on the users
    terminal size

---
+++ pascal.py
@@ -0,0 +1,27 @@
+import os
+
+width = os.get_terminal_size().columns
+
+line = [1]
+
+line_view = ""
+
+print("\033[2J\033[0;0H")
+
+def new_line(old):
+    new_line = []
+    a = 0
+    b = 1
+    new_line.append(old[0])
+    for i in range(len(old) - 1):
+        new_line.append(old[a] + old[b])
+        a += 1
+        b += 1
+    new_line.append(old[-1])
+    return new_line
+
+while width >= len(line_view):
+    print(line_view.center(width))
+    line_str = [str(num) for num in line]
+    line_view = "   ".join(line_str)
+    line = new_line(line)
